abcd054f7a62663a0f9566ee4f893ac34600863c
OrmandyRony <ormandyrony@ieee.org>
2022-09-14 14:09:46 -0600

    Se inicia la version 2 del proyecto 1

---
+++ Proyecto1V2/src/proyecto1v2/Proyecto1V2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1v2;
+
+/**
+ *
+ * @author ormandyrony
+ */
+public class Proyecto1V2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
