229a0db62916bde6b2f9a9f0f521664c4c75623c
OrmandyRony <ormandyrony@ieee.org>
2022-09-18 05:39:38 -0600

    Se agrego la clase Mientras, y se compusieron errores de las expresiones aritmeticas

---
+++ Proyecto1V2/src/Estructuras/Instrucciones/Mientras.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras.Instrucciones;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author ormandyrony
+ */
+public class Mientras implements Instruccion {
+    private final Operacion condicion;
+    private LinkedList<Instruccion> listaInstrucciones;
+    
+    /**
+     * Cuando viene sin instrucciones
+     * @param condicion 
+     */
+    public Mientras(Operacion condicion) {
+        this.condicion = condicion;
+    }
+
+    /**
+     * Con instrucciones
+     * @param condicion
+     * @param listaInstrucciones 
+     */
+    public Mientras(Operacion condicion, LinkedList<Instruccion> listaInstrucciones) {
+        this.condicion = condicion;
+        this.listaInstrucciones = listaInstrucciones;
+    }
+    
+    @Override
+    public String traducir() {
+        String traduccion = "while (" + this.condicion.traducir() + "):\n";
+        
+        if (listaInstrucciones != null) {
+            for (Instruccion instruccion : listaInstrucciones) {
+                traduccion += "\t";
+                traduccion += instruccion.traducir();
+            }
+        }
+        
+        
+        return traduccion;
+    }
+    
+}
