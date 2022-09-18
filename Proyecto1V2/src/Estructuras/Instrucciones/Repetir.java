b0c85c441c59adae17274902861ac5d173e9ee2a
OrmandyRony <ormandyrony@ieee.org>
2022-09-18 08:25:46 -0600

    Se agrego la clase Repeir y la gramatica en el cup

---
+++ Proyecto1V2/src/Estructuras/Instrucciones/Repetir.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras.Instrucciones;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author ormandyrony
+ */
+public class Repetir implements Instruccion {
+    private LinkedList<Instruccion> listaInstrucciones;
+    private final Operacion condicion;
+
+    public Repetir(Operacion condicion) {
+        this.condicion = condicion;
+    }
+
+    public Repetir(LinkedList<Instruccion> listaInstrucciones, Operacion condicion) {
+        this.listaInstrucciones = listaInstrucciones;
+        this.condicion = condicion;
+    }
+
+    @Override
+    public String traducir() {
+        String traduccion = "valor = True\n";
+        traduccion += "while (valor == True):\n";
+        
+        if (listaInstrucciones != null) {
+            for (Instruccion instruccion : listaInstrucciones) {
+                traduccion += "\t";
+                traduccion += instruccion.traducir();
+            }
+        }
+        
+        traduccion += "\tif " + this.condicion.traducir() + ":\n\tbreak";
+        
+        return traduccion;
+    }
+    
+    
+}
