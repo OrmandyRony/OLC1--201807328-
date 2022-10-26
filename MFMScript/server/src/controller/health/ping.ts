ab0f17952197d104325d9ed2760ffdc62de5c32e
OrmandyRony <ormandyrony@ieee.org>
2022-10-25 20:17:08 -0600

    Instalacion de Jison y creacion de app en react

--- MFMScript/server/src/controller/health/ping.ts
+++ MFMScript/server/src/controller/health/ping.ts
@@ -3,10 +3,10 @@ import { Response, Request } from "express";
 const {name, version, description } = require('../../../package.json');
 
 export const ping = (req: Request, res: Response): void => {
-    res.json({
-        name,
-        description,
-        version,
-        uptime: process.uptime()
-    })
+  res.json({
+    name,
+    description,
+    version,
+    uptime: process.uptime()
+  })
 }
